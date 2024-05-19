a627ac69780b8ba1b9d14658a101d4b4bfa2120e
haytham818 <haytham818@outlook.com>
2024-05-19 22:46:27 +0800

    changeavatar

--- src/components/blocks/avatar/image.tsx
+++ src/components/blocks/avatar/image.tsx
@@ -8,22 +8,22 @@ import avatarImg from '@/images/avatar.webp'
 
 import { useIsServer } from '@/hooks/useIsServer'
 
-// export const AvatarImage = () => {
-//   const isServer = useIsServer()
+export const AvatarImage = () => {
+  const isServer = useIsServer()
 
-//   return (
-//     <Image
-//       className={clsx(
-//         'absolute inset-x-0 bottom-0 z-10 origin-bottom transition-transform',
-//         !isServer && 'scale-110',
-//       )}
-//       priority
-//       loading='eager'
-//       src={avatarImg}
-//       alt='avatar'
-//       placeholder='empty'
-//       width={280}
-//       height={280}
-//     />
-//   )
-// }
+  return (
+    <Image
+      className={clsx(
+        'absolute inset-x-0 bottom-0 z-10 origin-bottom transition-transform',
+        !isServer && 'scale-110',
+      )}
+      priority
+      loading='eager'
+      src={avatarImg}
+      alt='avatar'
+      placeholder='empty'
+      width={280}
+      height={280}
+    />
+  )
+}
